aab017a4c54dba798716ea1085b5784f6134c735
Filippo Greco s309529 <s309529@studenti.polito.it>
2024-02-20 23:05:52 +0100

    edit - aggregation + LSTM

--- models/LSTMClassifier.py
+++ models/LSTMClassifier.py
@@ -9,24 +9,22 @@ class LSTMClassifier(nn.Module):
         [TODO]: the classifier should be implemented by the students and different variations of it can be tested
         in order to understand which is the most performing one """
         
-        self.conv1 = nn.Conv1d(1, 32, 3, 1, 1)
-        
-        self.lstm = nn.LSTM(input_size*32, hidden_size, 1, batch_first=True)
+        # TODO: try dropout
+        self.lstm = nn.LSTM(input_size, hidden_size, 1, batch_first=True)
         
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(hidden_size, num_classes)
-        
-        self.h0 = lambda x: torch.zeros(1, hidden_size).to(x.device)
-        self.c0 = lambda x: torch.zeros(1, hidden_size).to(x.device)
 
     def forward(self, x):
-        out = x.reshape(-1, 1, 1024)
-        out = self.conv1(out)
-        out = self.relu(out)
-        x = out.reshape(-1, 32*1024)
-        
-        out, _ = self.lstm(x, (self.h0(x), self.c0(x)))
-        out = self.relu(out)
+        x = x.reshape((x.shape[0], 5, 1024))
+
+        # initialize the hidden state.
+        h0 = torch.zeros(1, x.shape[0], 512).to(x.device)
+        c0 = torch.zeros(1, x.shape[0], 512).to(x.device)
+        state = (h0, c0)
+
+        out, state = self.lstm(x, state)
+        out = self.relu(out[:, -1, :])
         out = self.fc1(out)
         
         return out, {}
