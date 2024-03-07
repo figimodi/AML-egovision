b9fa2a9071c07bc7188b6efacb48871ea6bc1d1c
Filippo Greco s309529 <s309529@studenti.polito.it>
2024-03-07 15:32:07 +0100

    add torch.MLP, add LDA

--- models/MLP.py
+++ models/MLP.py
@@ -1,23 +1,28 @@
 from torch import nn
-
+from torchvision import ops
 
 class MLP(nn.Module):
     def __init__(self, num_classes, input_size=1024, hidden_size=512):
         super().__init__()
 
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, num_classes)
-        self.sigmoid = nn.Sigmoid()
+        # using torchvision built in MLP
+        self.clf = ops.MLP(in_channels=input_size, hidden_channels=[hidden_size, hidden_size, num_classes], dropout=0.5)
+
+        # self.relu = nn.ReLU()
+        # self.fc1 = nn.Linear(input_size, hidden_size)
+        # self.fc2 = nn.Linear(hidden_size, hidden_size)
+        # self.fc3 = nn.Linear(hidden_size, num_classes)
+        # self.sigmoid = nn.Sigmoid()
         
-        self.classifier = nn.Sequential(
-            self.fc1,
-            self.relu,
-            self.fc2,
-            self.relu,
-            self.fc3,
-        )
+        # self.classifier = nn.Sequential(
+        #     self.fc1,
+        #     self.relu,
+        #     self.fc2,
+        #     self.relu,
+        #     self.fc3,
+        # )
+
+        self.classifier = self.clf
 
     def forward(self, x):
         return self.classifier(x), {}
--- plot_features.py
+++ plot_features.py
@@ -2,13 +2,14 @@ import os
 import pickle as pk
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from omegaconf import OmegaConf
 from collections import defaultdict
 
-def plot_features(args):
+def plot_features_PCA(args):
     features_path = args.get('features_path', os.path.join('./saved_features/SAVE_DENSE-extracted_D1_test.pkl'))
     samples_path = args.get('split_path', os.path.join('./train_val/D1_test.pkl'))
     base_image_path = args.get('images_path', os.path.join('../ek_data/frames/'))
@@ -113,7 +114,60 @@ def plot_features(args):
     plt.savefig(output_image_path, dpi=300)
     plt.show()
 
+
+def plot_features_LDA(args):
+    features_path = args.get('features_path', os.path.join('./saved_features/SAVE_DENSE-extracted_D1_test.pkl'))
+    samples_path = args.get('split_path', os.path.join('./train_val/D1_test.pkl'))
+    base_image_path = args.get('images_path', os.path.join('../ek_data/frames/'))
+    output_image_path = args.get('output_image_path', os.path.join('plots/test.png'))
+
+    NUM_CLASSES = 8
+    reduced_features = []
+    labels = []
+
+    with open(features_path, 'rb') as f_file:
+        data = pk.load(f_file)
+        lda = LinearDiscriminantAnalysis(n_components=2)
+        
+        with open(samples_path, 'rb') as s_file:
+            samples = pk.load(s_file)
+        
+        # dictionary of label: list_of_action
+        label_actions = defaultdict(set)
+        for idx in range(len(samples)):
+            label_actions[samples['verb_class'][idx]].add(samples['verb'][idx])
+
+        for label, acts in label_actions.items():
+            label_actions[label] = ', '.join(acts)
+
+        extracted_samples = np.array([x['features_RGB'] for x in data['features']])
+        extracted_samples = np.mean(extracted_samples, 1)
+        labels = samples['verb_class']
+
+    extracted_samples = lda.fit_transform(extracted_samples, labels)
+    extracted_samples = np.array(extracted_samples)
+    extracted_samples = extracted_samples.reshape(extracted_samples.shape[0],  2)
+    
+    color = iter(plt.cm.rainbow(np.linspace(0, 1, NUM_CLASSES)))
+
+    for label in range(NUM_CLASSES):
+        cl = next(color)
+        class_samples = np.array([extracted_samples[j] for j in range(len(extracted_samples)) if labels[j] == label])
+        plt.scatter(class_samples[:, 0], class_samples[:, 1], c=cl, label=label_actions[label])
+
+    plt.legend(fontsize='small', loc='upper right')
+    plt.gcf().set_size_inches(10, 7)
+    plt.savefig(output_image_path, dpi=300)
+    plt.show()
+
+
 if __name__ == '__main__':
     cli_args = OmegaConf.from_cli()
     print(cli_args)
-    plot_features(cli_args)
+    # plot_features_PCA(cli_args)
+    plot_features_LDA(cli_args)
+
+    plot_features_
+
+    # TODO: try LDA, isomap, t-sne (scikitlearn)
+    # TODO: fix code PCA (remove for loop for d in data[...], take a look at LDA code)
