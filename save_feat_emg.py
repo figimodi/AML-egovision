676f08aa2960dc2dbb274223762c85bb5f435c31
Filippo Greco s309529 <s309529@studenti.polito.it>
2024-04-03 17:34:41 +0200

    augmented emg+rgb and split train+test

--- save_feat_emg.py
+++ save_feat_emg.py
@@ -6,6 +6,28 @@ import numpy as np
 import pandas as pd
 import os
 
+emg_descriptions_to_labels = [
+    'Clean a pan with a sponge',
+    'Clean a pan with a towel',
+    'Clean a plate with a sponge',
+    'Clean a plate with a towel',
+    'Clear cutting board',
+    'Get items from cabinets: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Get/replace items from refrigerator/cabinets/drawers',
+    'Load dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Open/close a jar of almond butter',
+    'Peel a cucumber',
+    'Peel a potato',
+    'Pour water from a pitcher into a glass',
+    'Set table: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Slice a cucumber',
+    'Slice a potato',
+    'Slice bread',
+    'Spread almond butter on a bread slice',
+    'Spread jelly on a bread slice',
+    'Stack on table: 3 each large/small plates, bowls',
+    'Unload dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+]
 
 def chunk_timestamps_and_readings(left_timestamps, left_readings, right_timestamps, right_readings):
     # Initialize empty lists to store the chunks and readings
@@ -48,12 +70,30 @@ def chunk_timestamps_and_readings(left_timestamps, left_readings, right_timestam
     
     return left_chunks, right_chunks, left_reading_chunks, right_reading_chunks
 
-def augment_dataset(file_path: str):
+def augment_dataset():
+    emg_folder = 'emg/'
+    action_folder = 'action-net/'
+
+    for filename in os.listdir(action_folder):
+        if os.path.isfile(os.path.join(action_folder, filename)):
+            if 'augmented' in filename.lower():
+                os.remove(os.path.join(action_folder, filename))
+
+    for filename in os.listdir(emg_folder):
+        if os.path.isfile(os.path.join(emg_folder, filename)):
+            if 'augmented' in filename.lower():
+                os.remove(os.path.join(emg_folder, filename))
+
+    partitions = os.listdir(emg_folder)
+    for p in partitions:
+        augment_partition(os.path.join(emg_folder, p))
+
+def augment_partition(file_path: str):
     data = pd.DataFrame(pd.read_pickle(file_path))
     new_data = []
 
     for i, _ in data.iterrows():
-        if i == 0:
+        if data.loc[i, 'description'] == 'calibration':
             continue
         left_timestamps_chunks, right_timestamps_chunks, left_readings_chunks, right_readings_chunks = chunk_timestamps_and_readings(data.loc[i, 'myo_left_timestamps'], data.loc[i, 'myo_left_readings'], data.loc[i, 'myo_right_timestamps'], data.loc[i, 'myo_right_readings'])
 
@@ -67,7 +107,14 @@ def augment_dataset(file_path: str):
         n_chunks = len(left_timestamps_chunks)
 
         for c in range(n_chunks):
+            # handle old descriptions
+            if data.loc[i, 'description'] == 'Get items from refrigerator/cabinets/drawers':
+                data.at[i, 'description'] = 'Get/replace items from refrigerator/cabinets/drawers'
+            if data.loc[i, 'description'] == 'Open a jar of almond butter':
+                data.at[i, 'description'] = 'Open/close a jar of almond butter'
+            
             new_row = {
+                'old_index': i,
                 'description': data.loc[i, 'description'],
                 'start': min(left_timestamps_chunks[c][0], right_timestamps_chunks[c][0]),
                 'stop': min(left_timestamps_chunks[c][-1], right_timestamps_chunks[c][-1]),
@@ -85,37 +132,117 @@ def augment_dataset(file_path: str):
     file_name, file_extension = os.path.splitext(file_path)
     new_data.to_pickle(f'{file_name}_augmented.pkl')
 
-    # TODO: in this phase, while augmentig, we need to also create the new ActionNet_test and ActionNet_train that contains the new splits of the new augmented dataset
-    # TODO: replace "Get items from refrigerator/cabinets/drawers" to "Get/replace items from refrigerator/cabinets/drawers" and "Open a jar of almond butter" to "Open/close a jar of almond butter" to obtain 20 labels
+    # Update the split files with the new augmented dataset
+    create_split_augmented(file_path, f'{file_name}_augmented.pkl')
+    print(f'{file_path} was correctly augmented')
 
-# TODO: code to be used to prepare spilt train and test for rgb action net
-# def setup_pickle_rgb(file_path: str):
-#     data = pd.DataFrame(pd.read_pickle(file_path))
-#     new_data = []
+def create_split_augmented(old_file_path: str, new_file_path: str):
+    data = pd.DataFrame(pd.read_pickle(new_file_path))
+    if os.path.isfile('action-net/ActionNet_test_augmented.pkl'):
+        test = pd.DataFrame(pd.read_pickle('action-net/ActionNet_test_augmented.pkl'))
+    else:
+        test = pd.DataFrame(pd.read_pickle('action-net/ActionNet_test.pkl'))
 
-#     for i, _ in data.iterrows():
-#         new_row = {
-#             'uid': i,
-#             'participant_id': 'P04',
-#             'video_id': 'P04_01',
-#             'narration': data.loc[i, 'description'],
-#             'start_timestamp': data.loc[i, 'start'], # TODO: remove t0
-#             'stop_timestamp': data.loc[i, 'stop'], # TODO: remove t0
-#             'start_frame': data.loc[i, 'start'] * 29.67, # TODO: remove t0
-#             'stop_frame': data.loc[i, 'stop'] * 29.67, # TODO: remove t0
-#             'verb': data.loc[i, 'description'],
-#             'verb_class': emg_descriptions_to_labels.index(data.loc[i, 'description']),
-#         }
+    if os.path.isfile('action-net/ActionNet_train_augmented.pkl'):
+        train = pd.DataFrame(pd.read_pickle('action-net/ActionNet_train_augmented.pkl'))
+    else:
+        train = pd.DataFrame(pd.read_pickle('action-net/ActionNet_train.pkl'))
 
-#         new_data.append(new_row)
+    old_file_name = old_file_path.split('/')[1]
+    new_file_name = new_file_path.split('/')[1]
 
-#     # Convert the list of dictionaries to a DataFrame
-#     new_data = pd.DataFrame(new_data)
+    # mapping each old index to the list of new (augmented) indices
+    mapping_new_index = {}
+    for i, _ in data.iterrows():
+        old_index = data.loc[i, 'old_index']
+        new_index = i
+        
+        if old_index in mapping_new_index:
+            mapping_new_index[old_index].append(new_index)
+        else:
+            mapping_new_index[old_index] = [new_index]
 
-#     # Save the DataFrame to a pickle file
-#     file_name, file_extension = os.path.splitext(file_path)
-#     new_data.to_pickle(f'{file_name}_rgb.pkl')
+    # TEST: creating the new rows of the new file split that refer to the augmented dataset
+    old_test_rows = test[test['file'] == old_file_name]
+    new_test_rows = []
+    new_data = []
+    for i, _ in old_test_rows.iterrows():
+        old_index = old_test_rows.loc[i, 'index']
+        for new_index in mapping_new_index[old_index]:
+            # handle old descriptions
+            if old_test_rows.loc[i, 'description'] == 'Get items from refrigerator/cabinets/drawers':
+                old_test_rows.at[i, 'description'] = 'Get/replace items from refrigerator/cabinets/drawers'
+            if old_test_rows.loc[i, 'description'] == 'Open a jar of almond butter':
+                old_test_rows.at[i, 'description'] = 'Open/close a jar of almond butter'
+            new_row = {
+                'index': int(new_index),
+                'file': new_file_name,
+                'description': old_test_rows.loc[i, 'description'],
+                'labels': old_test_rows.loc[i, 'labels']
+            }
+            new_data.append(new_row)
 
+    new_test_rows = test.drop(test[test['file'] == old_file_name].index)
+    new_test_rows = pd.concat([new_test_rows, pd.DataFrame(new_data)])
+
+    # TRAIN: creating the new rows of the new file split that refer to the augmented dataset
+    old_train_rows = train[train['file'] == old_file_name]
+    new_train_rows = []
+    new_data = []
+    for i, _ in old_train_rows.iterrows():
+        old_index = old_train_rows.loc[i, 'index']
+        for new_index in mapping_new_index[old_index]:
+            # handle old descriptions
+            if old_train_rows.loc[i, 'description'] == 'Get items from refrigerator/cabinets/drawers':
+                old_train_rows.at[i, 'description'] = 'Get/replace items from refrigerator/cabinets/drawers'
+            if old_train_rows.loc[i, 'description'] == 'Open a jar of almond butter':
+                old_train_rows.at[i, 'description'] = 'Open/close a jar of almond butter'
+            new_row = {
+                'index': int(new_index),
+                'file': new_file_name,
+                'description': old_train_rows.loc[i, 'description'],
+                'labels': old_train_rows.loc[i, 'labels']
+            }
+            new_data.append(new_row)
+
+    new_train_rows = train.drop(train[train['file'] == old_file_name].index)
+    new_train_rows = pd.concat([new_train_rows, pd.DataFrame(new_data)])
+
+    # Save the DataFrame to a pickle file
+    new_test_rows.to_pickle(f'action-net/ActionNet_test_augmented.pkl')
+    new_train_rows.to_pickle(f'action-net/ActionNet_train_augmented.pkl')
+
+def emg2rgb():
+    emg_folder = 'emg/'
+    partitions = os.listdir(emg_folder)
+
+    for filename in partitions:
+        if os.path.isfile(os.path.join(emg_folder, filename)) and 'augmented' in filename.lower():
+            data = pd.DataFrame(pd.read_pickle(os.path.join(emg_folder, filename)))
+            new_data = []
+
+            for i, _ in data.iterrows():
+                new_row = {
+                    'uid': i,
+                    'participant_id': 'P04',
+                    'video_id': 'P04_01',
+                    'narration': data.loc[i, 'description'],
+                    'start_timestamp': data.loc[i, 'start'],
+                    'stop_timestamp': data.loc[i, 'stop'],
+                    'start_frame': data.loc[i, 'start'] * 29.67,
+                    'stop_frame': data.loc[i, 'stop'] * 29.67,
+                    'verb': data.loc[i, 'description'],
+                    'verb_class': emg_descriptions_to_labels.index(data.loc[i, 'description']),
+                }
+                new_data.append(new_row)
+
+            # Convert the list of dictionaries to a DataFrame
+            new_data = pd.DataFrame(new_data)
+
+            # Save the DataFrame to a pickle file
+            file_name, file_extension = os.path.splitext(filename)
+            new_data.to_pickle(f'{emg_folder}/{file_name}_rgb.pkl')
+            print(f'{filename} succesfully produced the rgb counterpart')
 
 def emg_adjust_features(file_path: str, *, cut_frequency: float = 5.0, filter_order: int = 4):
     data = pd.DataFrame(pd.read_pickle(file_path))
@@ -124,7 +251,7 @@ def emg_adjust_features(file_path: str, *, cut_frequency: float = 5.0, filter_or
     # Absolute value of the readings and application of low pass filter
     for side in to_read:
         for i, _ in data.iterrows():
-            if i != 0:
+            if data.loc[i, 'description'] != 'calibration':
                 # Rectify channels
                 row = abs(data.loc[i, side + '_readings'])
                 times = data.loc[i, side + '_timestamps']
@@ -151,7 +278,7 @@ def emg_adjust_features(file_path: str, *, cut_frequency: float = 5.0, filter_or
         l2norm = np.linalg.norm(flat_side_data)
         
         for i, _ in data.iterrows():
-            if i != 0:
+            if data.loc[i, 'description'] != 'calibration':
                 row = data.loc[i, side + '_readings']
                 normalized = row / l2norm
                 
@@ -164,7 +291,7 @@ def emg_adjust_features(file_path: str, *, cut_frequency: float = 5.0, filter_or
         mn = min(flat_side_data)
         
         for i, _ in data.iterrows():
-            if i != 0:
+            if data.loc[i, 'description'] != 'calibration':
                 row = data.loc[i, side + '_readings']
                 # Normalize and shift in [-1, 1]
                 normalized = (row - mn) * 2 / (mx-mn) - 1
@@ -211,7 +338,8 @@ def emg_adjust_features_index(file_path: str, index: int, *, cut_frequency: floa
     return transformed
 
 if __name__ == '__main__':
-    prova()            
+    # augment_dataset()
+    emg2rgb()
 
 # TODO: spectogram
 # TODO: saples are not balanced maybe
--- utils/loaders.py
+++ utils/loaders.py
@@ -36,28 +36,26 @@ import numpy as np
 # 19. Unload dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils
 
 emg_descriptions_to_labels = [
-    'Clean a pan with a sponge'
-    'Clean a pan with a towel'
-    'Clean a plate with a sponge'
-    'Clean a plate with a towel'
-    'Clear cutting board'
-    'Get items from cabinets: 3 each large/small plates, bowls, mugs, glasses, sets of utensils'
-    'Get items from refrigerator/cabinets/drawers'
-    'Get/replace items from refrigerator/cabinets/drawers'
-    'Load dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils'
-    'Open a jar of almond butter'
-    'Open/close a jar of almond butter'
-    'Peel a cucumber'
-    'Peel a potato'
-    'Pour water from a pitcher into a glass'
-    'Set table: 3 each large/small plates, bowls, mugs, glasses, sets of utensils'
-    'Slice a cucumber'
-    'Slice a potato'
-    'Slice bread'
-    'Spread almond butter on a bread slice'
-    'Spread jelly on a bread slice'
-    'Stack on table: 3 each large/small plates, bowls'
-    'Unload dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils'
+    'Clean a pan with a sponge',
+    'Clean a pan with a towel',
+    'Clean a plate with a sponge',
+    'Clean a plate with a towel',
+    'Clear cutting board',
+    'Get items from cabinets: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Get/replace items from refrigerator/cabinets/drawers',
+    'Load dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Open/close a jar of almond butter',
+    'Peel a cucumber',
+    'Peel a potato',
+    'Pour water from a pitcher into a glass',
+    'Set table: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
+    'Slice a cucumber',
+    'Slice a potato',
+    'Slice bread',
+    'Spread almond butter on a bread slice',
+    'Spread jelly on a bread slice',
+    'Stack on table: 3 each large/small plates, bowls',
+    'Unload dishwasher: 3 each large/small plates, bowls, mugs, glasses, sets of utensils',
 ]
 
 # class ActionNetRgbDataset(data.Dataset, ABC):
@@ -67,6 +65,7 @@ emg_descriptions_to_labels = [
     
 #     def __getitem__(self, index):
 #         # TODO: to implement
+          # TODO: when loading frame remove t0, which is equal to true_start_video*29,67
 #         return 
 
 #     def __len__(self):
