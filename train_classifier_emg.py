6f65f3624c732a19c03902fc70c692cddecb2775
stiven-hidri <hidristiven@gmail.com>
2024-05-07 13:47:53 +0200

    asd

--- train_classifier_emg.py
+++ train_classifier_emg.py
@@ -258,12 +258,10 @@ def validate(model, val_loader, device, it, num_classes):
             model.compute_accuracy(logits, label)
 
             if (i_val + 1) % (len(val_loader) // 5) == 0:
-                logger.info("[{}/{}] top1= {:.3f}% top5 = {:.3f}%".format(i_val + 1, len(val_loader),
-                                                                          model.accuracy.avg[1], model.accuracy.avg[5]))
+                logger.info("[{}/{}] top1= {:.3f}% top5 = {:.3f}%".format(i_val + 1, len(val_loader), model.accuracy.avg[1], model.accuracy.avg[5]))
 
-        class_accuracies = [(x / y) * 100 for x, y in zip(model.accuracy.correct, model.accuracy.total)]
-        logger.info('Final accuracy: top1 = %.2f%%\ttop5 = %.2f%%' % (model.accuracy.avg[1],
-                                                                      model.accuracy.avg[5]))
+        class_accuracies = [(x / y) * 100 for x, y in zip(model.accuracy.correct, model.accuracy.total) if y>0]
+        logger.info('Final accuracy: top1 = %.2f%%\ttop5 = %.2f%%' % (model.accuracy.avg[1], model.accuracy.avg[5]))
         for i_class, class_acc in enumerate(class_accuracies):
             logger.info('Class %d = [%d/%d] = %.2f%%' % (i_class,
                                                          int(model.accuracy.correct[i_class]),
