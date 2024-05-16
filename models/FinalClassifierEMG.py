ad00ff418cdc47d97dff5500827aeccb5be074a7
Filippo Greco s309529 <s309529@studenti.polito.it>
2024-05-16 15:50:15 +0200

    funziona tutto

--- models/FinalClassifierEMG.py
+++ models/FinalClassifierEMG.py
@@ -6,15 +6,17 @@ class FinalClassifierEMG(nn.Module):
         self.hidden_size1 = hidden_size1
         self.hidden_size2 = hidden_size2
 
-        self.lstm1 = nn.LSTM(input_size, hidden_size1, batch_first=True)
-        self.lstm2 = nn.LSTM(hidden_size1, hidden_size2, batch_first=True)
+        # self.lstm1 = nn.LSTM(input_size, hidden_size1, batch_first=True)
+        self.lstm2 = nn.LSTM(input_size, hidden_size2, batch_first=True)
         self.dropout = nn.Dropout(p=dropout)
+        self.relu = nn.ReLU(inplace=True)
         self.dense = nn.Linear(hidden_size2, num_classes)
 
     def forward(self, x):
-        out, _ = self.lstm1(x)
-        out, _ = self.lstm2(out)
+        # out, _ = self.lstm1(x)
+        out, _ = self.lstm2(x)
         out = self.dropout(out[:, -1, :])
+        out = self.relu(out)
         out = self.dense(out)
         
         return out, {}
