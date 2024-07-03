b4d0fa2b91ef59046e3cdeda07ba4c13a9cbca79
stiven-hidri <hidristiven@gmail.com>
2024-07-03 15:32:09 +0200

    momentarely added script for viewing spectrograms

---
+++ show_specto.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from PIL import Image
+
+img = torch.load('..\\spectrograms\\S00_2_0.pt')
+
+for index, i in enumerate(img):
+    arr = i.detach().cpu().numpy()[0]*255.   
+    im = Image.fromarray(arr).convert('L')
+    im.save(f"tmp\\{index}.jpg")
